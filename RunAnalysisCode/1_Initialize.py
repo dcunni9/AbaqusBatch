c94a64ab3f62b4350279842badcdde76f62b4665
David Cunningham <80409181+dcunni9@users.noreply.github.com>
2022-03-16 13:08:24 -0400

    added citation

--- RunAnalysisCode/1_Initialize.py
+++ RunAnalysisCode/1_Initialize.py
@@ -1,8 +1,11 @@
+import os
+#IT'S IMPORTANT THAT YOU ONLY RUN THIS ONCE, OR YOU WILL LOSE ALL YOUR WORK! 
 #Indicate a general folder in the DATA drive (drive with largest available memory) where you would like your code to work in. 
-usersubfolder = D:/Users/HULCUSER/dcunni9/AbaqusData
+usersubfolder = "D:/Users/HULCUSER/dcunni9/AbaqusData"
 
 #this portion of the code will create your MultiQueuedFiles, QueryOdb_oneinpfileonly, and DataArchive folders. 
-import os
+
+
 
 os.mkdir(usersubfolder + "/MultiQueuedFiles")
 os.mkdir(usersubfolder + "/QueryOdb_oneinpfileonly")
